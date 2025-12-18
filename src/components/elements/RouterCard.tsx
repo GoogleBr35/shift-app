35c6429527c87e89f53931c6f508ccdaa21b347e
GoogleBr35 <soyasato15@gmail.com>
2025-12-18 15:54:38 +0900

    [modify] MenuCard -> RouterCardへリネーム

--- src/components/elements/RouterCard.tsx
+++ src/components/elements/RouterCard.tsx
@@ -1,14 +1,27 @@
-import { Button } from '@/components/elements';
+'use client';
 
-export default function MenuCard({
+import { Button } from '@/components/elements';
+import { useRouter } from 'next/navigation';
+
+export default function RouterCard({
     title,
     description,
     label,
+    path,
 }: {
     title: string;
     description: string;
     label: string;
+    path?: string;
 }) {
+    const router = useRouter();
+
+    const handleClick = () => {
+        if (path) {
+            router.push(path);
+        }
+    };
+
     return (
         <div className="w-full max-w-sm rounded-lg outline outline-gray-200 bg-white p-6 shadow-lg">
             <div className="flex flex-col gap-2">
@@ -16,7 +29,10 @@ export default function MenuCard({
                 <p className="text-sm text-gray-500">{description}</p>
 
                 <div className="mt-4 flex justify-end">
-                    <Button className="bg-gray-800! px-6! py-1! text-white hover:bg-gray-900!">
+                    <Button
+                        className="bg-gray-800! px-6! py-1! text-white hover:bg-gray-900!"
+                        onClick={handleClick}
+                    >
                         {label}
                     </Button>
                 </div>
--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from './Button';
 export { default as Input } from './Input';
-export { default as MenuCard } from './MenuCard';
+export { default as RouterCard } from './RouterCard';
