5cefb78fd4853e5c0afc74a6ce10313b2df8c66d
GoogleBr35 <soyasato15@gmail.com>
2025-12-19 13:44:00 +0900

    [modify] スマホ用UIのリファクタリングと属性の追加
    ## リファクタリング
    * descriptionのwhitespace pre-wrap
    * マージンの修正
    * ボタンサイズ変更
    
    ## 属性の追加
    * buttonDisabled: カード内のボタンをインタラクト不可にするboolean型のprop
    * onClick: pathに遷移する前に、ボタンクリック時に発火したい処理を渡すためのprop

--- src/components/elements/RouterCard.tsx
+++ src/components/elements/RouterCard.tsx
@@ -8,15 +8,22 @@ export default function RouterCard({
     description,
     label,
     path,
+    buttonDisabled = false,
+    onClick,
 }: {
     title: string;
     description: string;
     label: string;
     path?: string;
+    buttonDisabled?: boolean;
+    onClick?: () => void | Promise<void>;
 }) {
     const router = useRouter();
 
-    const handleClick = () => {
+    const handleButtonClick = async () => {
+        if (onClick) {
+            await onClick();
+        }
         if (path) {
             router.push(path);
         }
@@ -26,12 +33,19 @@ export default function RouterCard({
         <div className="w-full max-w-sm rounded-lg outline outline-gray-200 bg-white p-6 shadow-lg">
             <div className="flex flex-col gap-2">
                 <h3 className="text-lg font-bold text-gray-900">{title}</h3>
-                <p className="text-sm text-gray-500">{description}</p>
+                <p className="text-sm text-gray-500 whitespace-pre-wrap">
+                    {description}
+                </p>
 
-                <div className="mt-4 flex justify-end">
+                <div className="mt-6 flex justify-end">
                     <Button
-                        className="bg-gray-800! px-6! py-1! text-white hover:bg-gray-900!"
-                        onClick={handleClick}
+                        className={`w-full sm:w-auto px-8! py-3! text-base! font-bold ${
+                            buttonDisabled
+                                ? 'bg-gray-400 cursor-not-allowed'
+                                : 'bg-gray-800! hover:bg-gray-900! active:bg-gray-700!'
+                        }`}
+                        onClick={handleButtonClick}
+                        disabled={buttonDisabled}
                     >
                         {label}
                     </Button>
