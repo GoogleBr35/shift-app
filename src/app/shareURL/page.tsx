99be38e8fb99a391a2039ecc6ac6de27743eabb4
GoogleBr35 <soyasato15@gmail.com>
2026-02-20 15:54:56 +0900

    [add] URL共有ページ

---
+++ src/app/shareURL/page.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/layouts/Header';
+import CenterCardLayout from '@/components/layouts/CenterCardLayout';
+
+export default function NewShift() {
+    return (
+        <>
+            <header>
+                <Header />
+            </header>
+            <main>
+                <CenterCardLayout>
+                    <h1>共有URL</h1>
+                </CenterCardLayout>
+            </main>
+        </>
+    );
+}
